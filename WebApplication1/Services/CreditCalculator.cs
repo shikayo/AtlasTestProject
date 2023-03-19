f2f8de6453e44b8132b08b2e1fe73b9e28e0a682
shikayo <japanlover1337@gmail.com>
2023-03-19 21:44:17 +0300

    2

--- WebApplication1/Services/CreditCalculator.cs
+++ WebApplication1/Services/CreditCalculator.cs
@@ -4,23 +4,40 @@ namespace WebApplication1.Services;
 
 public class CreditCalculator : ICreditCalculator
 {
-    private readonly List<Result> _results=new List<Result>();
+    private readonly List<Result> _results;
+
+    public CreditCalculator()
+    {
+        _results = new List<Result>();
+    }
+
+    private double CalculatePayment(double amount,int loanDuration,double loanRate)
+    {
+        return (amount * Math.Pow(loanRate/100/12+1,loanDuration)*loanRate/100/12)
+               /(Math.Pow(loanRate/100/12+1,loanDuration)-1);
+    }
+
+    private double CalculatePaymentPerDay(double amount, int loanDurationDays, double loanRate, int paymentStep)
+    {
+        var paymentCount = loanDurationDays / paymentStep;
+        
+        return (amount * Math.Pow(loanRate/100/loanDurationDays+1,paymentCount)*loanRate/100/loanDurationDays)/(Math.Pow(loanRate/100/loanDurationDays+1,paymentCount)-1);
+
+    }
     
     public List<Result> CalculateResult(double amount,int loanDuration,double loanRate)
     {
-        loanRate /= 100;
-
-        var payment = (amount * Math.Pow(loanRate/12+1,loanDuration)*loanRate/12)/(Math.Pow(loanRate/12+1,loanDuration)-1);
+        var payment = CalculatePayment(amount, loanDuration, loanRate);
 
         for (int i = 0; i < loanDuration; i++)
         {
             var result = new Result
             {
                 Number = i + 1,
-                Date = DateOnly.Parse(DateTime.Today.ToShortDateString()).AddMonths(i),
+                Date = DateOnly.Parse(DateTime.Today.ToShortDateString()).AddMonths(i+1),
                 Amount = Math.Round(payment, 2),
-                Debt = Math.Round(payment - amount * loanRate / 12, 2),
-                Percentages = Math.Round(amount * loanRate / 12, 2)
+                Debt = Math.Round(payment - amount * loanRate /100/ 12, 2),
+                Percentages = Math.Round(amount * loanRate /100/ 12, 2)
             };
 
             result.RemainingDebt = Math.Round(amount - payment + result.Percentages, 1);
@@ -35,9 +52,7 @@ public class CreditCalculator : ICreditCalculator
 
     public double CalculateOverPayment(double amount, int loanDuration, double loanRate)
     {
-        loanRate /= 100;
-
-        var payment = (amount * Math.Pow(loanRate/12+1,loanDuration)*loanRate/12)/(Math.Pow(loanRate/12+1,loanDuration)-1);
+        var payment = CalculatePayment(amount, loanDuration, loanRate);
 
         var overPayment = payment * loanDuration - amount;
         return Math.Round(overPayment,2);
@@ -45,11 +60,9 @@ public class CreditCalculator : ICreditCalculator
 
     public double CalculateOverPaymentPerDay(double amount, int loanDurationDays, double loanRate, int paymentStep)
     {
-        loanRate /= 100;
-        
         var paymentCount = loanDurationDays / paymentStep;
-        
-        var payment = (amount * Math.Pow(loanRate/loanDurationDays+1,paymentCount)*loanRate/loanDurationDays)/(Math.Pow(loanRate/loanDurationDays+1,paymentCount)-1);
+
+        var payment = CalculatePaymentPerDay(amount, loanDurationDays, loanRate, paymentStep);
 
         var overPayment = 0.0;
         
@@ -58,19 +71,19 @@ public class CreditCalculator : ICreditCalculator
             
             for (int i = 0; i < paymentCount; i++)
             {
-                var percentages = Math.Round(amount * loanRate / paymentCount, 2);
+                var percentages = Math.Round(amount * loanRate/100 / paymentCount, 2);
                 overPayment += percentages;
                 amount = amount - payment + percentages;
             }
             
             var remainingDays = loanDurationDays % paymentStep;
-            var remainingPercentages = Math.Round(amount * Math.Pow(loanRate, remainingDays),2);
+            var remainingPercentages = Math.Round(amount * Math.Pow(loanRate/100, remainingDays),2);
 
-            overPayment += Math.Round(amount * Math.Pow(loanRate, remainingDays), 2);
+            overPayment += Math.Round(amount * Math.Pow(loanRate/100, remainingDays), 2);
         }
         else
         {
-            overPayment=payment * loanDurationDays - amount;
+            overPayment=payment * paymentCount - amount;
         }
 
         return Math.Round(overPayment,2);
@@ -86,12 +99,10 @@ public class CreditCalculator : ICreditCalculator
          *   • n - количество платежей (выражается в днях и приводится к количеству периодов с помощью деления на количество дней в году)
         */
         int i;
-
-        loanRate /= 100;
         
         var paymentCount = loanDurationDays / paymentStep;
-        
-        var payment = (amount * Math.Pow(loanRate/loanDurationDays+1,paymentCount)*loanRate/loanDurationDays)/(Math.Pow(loanRate/loanDurationDays+1,paymentCount)-1);
+
+        var payment = CalculatePaymentPerDay(amount, loanDurationDays, loanRate, paymentStep);
 
         for (i = 0; i < paymentCount; i++)
         {
@@ -100,8 +111,8 @@ public class CreditCalculator : ICreditCalculator
                 Number = i+1,
                 Date = DateOnly.Parse(DateTime.Today.AddDays(paymentStep*i).ToShortDateString()),
                 Amount = Math.Round(payment,2),
-                Debt = Math.Round(payment-amount*loanRate/paymentCount,2),
-                Percentages = Math.Round(amount * loanRate / paymentCount, 2)
+                Debt = Math.Round(payment-amount*loanRate/100/paymentCount,2),
+                Percentages = Math.Round(amount * loanRate/100 / paymentCount, 2)
             };
             result.RemainingDebt=Math.Round(amount - payment + result.Percentages, 1);
             
@@ -113,7 +124,7 @@ public class CreditCalculator : ICreditCalculator
         if (loanDurationDays % paymentStep != 0)
         {
             var remainingDays = loanDurationDays % paymentStep;
-            var remainingPercentages = Math.Round(amount * Math.Pow(loanRate, remainingDays),2);
+            var remainingPercentages = Math.Round(amount * Math.Pow(loanRate/100, remainingDays),2);
             var remainingPayment = Math.Round(amount + remainingPercentages,2);
             var mainDebt = Math.Round(remainingPayment - remainingPercentages,2);
             var remainDebt = Math.Round(remainingPayment - mainDebt - remainingPercentages,2);
